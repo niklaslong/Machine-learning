6f0f6348c60d3d6d373072b03666fd0cd63903a7
nkls <niklas@long.ch>
2017-06-23 13:54:29 -0700

    add line graph

--- genetic-algorithm/app.js
+++ genetic-algorithm/app.js
@@ -1,10 +1,10 @@
- 
+
 
 
 // BRAIN ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
 var brain = function() {
-  let dataArray = [[{x: 3,y: 4}, {x: 5,y: 7}], "x", "y"];
+  let dataArray = [[{x: 3,y: 4}, {x: 5,y: 7}, {x: 4,y: 5}], "x", "y"];
   draw(dataArray);
 };
 
@@ -13,65 +13,68 @@ var brain = function() {
  
  var draw = function(dataArray) {
 
-
-  let data = dataArray[0],
-      prop1 = dataArray[1],
-      prop2 = dataArray[2];
-
-
-  let margin = {top: 20, right: 20, bottom: 30, left: 50},
-      width = 1200 - margin.left - margin.right,
-      height = 600 - margin.top - margin.bottom;
+   let data = dataArray[0],
+       prop1 = dataArray[1],
+       prop2 = dataArray[2];
 
 
-  // set the ranges for x and y
-  let x = d3.scaleLinear().range([0, width]);
-  let y = d3.scaleLinear().range([height, 0]);
+  var svg = d3.select("svg"),
+    margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-  // append the svg object to the body of the page
-  // appends a 'group' element to 'svg'
-  // moves the 'group' element to the top left margin
-  var svg = d3.select("body").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
+  var x = d3.scaleLinear()
+    .range([0, width]);
 
-  //format the data
+  var y = d3.scaleLinear()
+    .range([height, 0]);
+
   data.forEach(function(d) {
-      d[prop1] = +d[prop1]; // formats whatever d.age is in d3.csv to number
-      d[prop2] = +d[prop2];
+      d.age = +d.age; // formats whatever d.age is in d3.csv to number
+      d.count = +d.count;
   });
 
-  // scale the range of the data
-  // d3.extent([1, 4, 3, 2]) -> [1, 4]
-  x.domain(d3.extent(data, function(d) { return d[prop1]; })).nice();
-  y.domain(d3.extent(data, function(d) { return d[prop2]; })).nice();
+  var line = d3.line()
+    .x(function(d) { return x(d[prop1]); })
+    .y(function(d) { return y(d[prop2]); });
 
+  x.domain(d3.extent(data, function(d) { return d[prop1]; }));
+  y.domain(d3.extent(data, function(d) { return d[prop2]; }));
 
-  // add the dots
-  svg.selectAll("dot")
-    .data(data)
-    .enter().append("circle")
-      .attr("r", 2.5)
-      .attr("cx", function(d) { return x(d[prop1]); })
-      .attr("cy", function(d) { return y(d[prop2]); })
-      .on("click", function() {
-        console.log(d);
-      });
-  
-  // add the X Axis
-  svg.append("g")
+  g.append("g")
       .attr("transform", "translate(0," + height + ")")
-      .attr("class", "x-axis")
-      .call(d3.axisBottom(x));
+        .call(d3.axisBottom(x))
+      .append("text")
+      .attr("fill", "#000")
+      .attr("dx", "0.71em")
+      .attr("y", 30)
+      .attr("x", width)
+      .attr("text-anchor", "start")
+      .text("x");
+    
 
-  // add the Y Axis
-  svg.append("g")
-      .attr("class", "y-axis")
-      .call(d3.axisLeft(y));
-};
+  g.append("g")
+      .call(d3.axisLeft(y))
+    .append("text")
+      .attr("fill", "#000")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end")
+      .text("y");
+
+  g.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("stroke-width", 1.5)
+      .attr("d", line);
+
+
+ };
 
 brain();
